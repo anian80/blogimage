015944a1fc2f186c7cf338d3424d2fc628237093
anian80 <a48263377@gmail.com>
2023-08-31 15:26:27 +0800

    修复python错误01

--- new_version.py
+++ new_version.py
@@ -4,11 +4,11 @@ import json;
 import time;
 
 
-with open("package.json",'r',encoding='utf-8')as f:
-	jspack json.load(f)	
+with open("package.json",'r',encoding='utf-8') as f:
+	jspack=json.load(f)	
 
-new_version '1.0.'+str(int(time.time()))[1:11:1]
+new_version = '1.0.'+str(int(time.time()))[1:11:1]
 jspack['version']=new_version
 
-with open("package.json",'w',encoding='utf-8')as f:
+with open("package.json",'w',encoding='utf-8') as f:
 	json.dump(jspack,f,ensure_ascii=False)
